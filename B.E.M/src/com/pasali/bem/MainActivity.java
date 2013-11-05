47748bfae194d74a9af8937c047a41c0651a6e6a
Mehmet Başal <mhmtbsl@bil.omu.edu.tr>
2013-11-05 21:30:51 +0200

    ask for ip

--- B.E.M/src/com/pasali/bem/MainActivity.java
+++ B.E.M/src/com/pasali/bem/MainActivity.java
@@ -1,11 +1,29 @@
 package com.pasali.bem;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	public static Socket socket;
+	private static final int PORT = 1238;
+	private static String serverIp;
+
+	public static String getServerIp() {
+		return serverIp;
+	}
+
+	public static void setServerIp(String serverIp) {
+		MainActivity.serverIp = serverIp;
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -14,12 +32,35 @@ public class MainActivity extends Activity {
 		Button bt = (Button) findViewById(R.id.button1);
 		bt.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-
-				// EditText et = (EditText) findViewById(R.id.editText1);
-				// String str = et.getText().toString();
-
+				/*
+				 * ip adresini al ve serverIp'ye ata
+				 */
+				EditText et = (EditText) findViewById(R.id.editText1);
+				String str = et.getText().toString();
+				setServerIp(str);
+				new Thread(new Client()).start();
+				Toast.makeText(getApplicationContext(),
+						str + " ip adresine bağlanıldı.", Toast.LENGTH_SHORT).show();
 			}
 		});
 
 	}
+
+	class Client implements Runnable {
+
+		@Override
+		public void run() {
+			try {
+				InetAddress serverAddr = InetAddress.getByName(serverIp);
+				socket = new Socket(serverAddr, PORT);
+			} catch (UnknownHostException e1) {
+				System.err.println("Bilinmeyen Sunucu");
+			} catch (IOException e1) {
+				System.err.println("Bağlantı Kurulamadı.");
+			}
+
+		}
+
+	}
+
 }
--- B.E.M/src/com/pasali/bem/RoutingService.java
+++ B.E.M/src/com/pasali/bem/RoutingService.java
@@ -4,10 +4,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
 import java.net.UnknownHostException;
-
 import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,20 +12,14 @@ import android.os.IBinder;
 
 public class RoutingService extends Service {
 
-	private Socket socket;
-	private static final int PORT = 1238;
-	private static final String serverIp = "192.168.1.107";
 	private String message;
+
 	@Override
 	public IBinder onBind(Intent arg0) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	@Override
-	public void onCreate() {
-		new Thread(new ClientThread()).start();	
-	}
-	@Override
+
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Bundle b = intent.getExtras();
 		if (b != null) {
@@ -37,44 +28,15 @@ public class RoutingService extends Service {
 		System.out.println(message);
 		try {
 			PrintWriter out = new PrintWriter(new BufferedWriter(
-					new OutputStreamWriter(socket.getOutputStream())), true);
+					new OutputStreamWriter(
+							MainActivity.socket.getOutputStream())), true);
 			out.println(message);
-			out.flush();
-			out.close();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (UnknownHostException e1) {
+			System.err.println("Bilinmeyen Sunucu");
+		} catch (IOException e1) {
+			System.err.println("Bağlantı Kurulamadı.");
 		}
 		return START_NOT_STICKY;
 	}
 
-	class ClientThread implements Runnable {
-
-		@Override
-		public void run() {
-
-			try {
-				InetAddress serverAddr = InetAddress.getByName(serverIp);
-				socket = new Socket(serverAddr, PORT);
-			} catch (UnknownHostException e1) {
-				e1.printStackTrace();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-
-		}
-
-	}
-	@Override
-	public void onDestroy() {
-		try {
-			socket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 }
